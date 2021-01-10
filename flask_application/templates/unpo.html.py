c3b8682dcd74c57f052c386012b3a3e09e1b68c3
Gabriel Shenouda <59508102+TheWoodkid@users.noreply.github.com>
2021-01-10 23:12:43 +0100

    Add files via upload

---
+++ flask_application/templates/unpo.html.py
@@ -0,0 +1,28 @@
+  <header class="intro-header" style="background-image: url('static/img/1509579.jpg')">
+ <h1 style= "color:#FF0000";> {{ prediction2 }} </h1>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+   <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+  <p style="color:#FF0000";> {{ prediction2 }} </p>
+ <p style="color:#FF0000";> {{ prediction2 }} </p>
+ 
+ 
+ 
+
